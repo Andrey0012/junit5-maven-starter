13d2a0b274999fefc7741ca79e02a508fa61e8b2
Author Ivan <Ivan.name@mail.com>
2023-03-02 22:18:34 +0300

    Initial commit

---
+++ src/main/java/com/dmdv/junet/TestLauncher.java
@@ -0,0 +1,8 @@
+package com.dmdv.junet;
+//import org.junit.platform.launcher.core.*;
+
+public class TestLauncher {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/dmdv/junet/service/UserServiceTest.java
@@ -0,0 +1,61 @@
+package com.dmdv.junet.service;
+
+import com.dmdv.junet.dto.User;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+ class UserServiceTest {
+     private UserService userService;
+    User IVAN = User.of(1, "Ivan", "123");
+    User PETR = User.of(2, "Petr", "456");
+
+
+
+    @BeforeAll
+     void init () {
+         System.out.println("Before all: "+this);
+     }
+
+     @BeforeEach
+     void prepare () {
+         System.out.println("Before each: " + this);
+         userService = new UserService();
+
+     }
+    @Test
+    void userEmptyIfNoUsersAdded () {
+        System.out.println("Test 1: " + this);
+        List<User> all = userService.getAll();
+        assertTrue(all.isEmpty());
+    }
+    @Test
+     void usersSizeIfUserAdded () {
+        System.out.println("Test 2: " + this);
+        userService.add(IVAN);
+        userService.add(PETR);
+        List<User> all = userService.getAll();
+        assertEquals(2, all.size());
+    }
+    @Test
+    void loginSuccessIfUserExists () {
+        userService.add(IVAN);
+        Optional<User> user = userService.login(IVAN.getUsername(), IVAN.getPassword());
+        assertTrue(user.isPresent());
+        user.ifPresent(user1 -> assertEquals(IVAN, user1));
+    }
+
+    @AfterEach
+     void deleteBase () {
+        System.out.println("After each: " + this);
+    }
+    @AfterAll
+    void closeEnd () {
+        System.out.println("After all: "+this);
+
+    }
+}
